7e8e3fc8888ba6423a6f0314c8f2bca12d0b05db
Игорь <mrigrek74@hotmail.com>
2018-07-31 12:24:47 +0500

    вдогонку

--- mrigrek74.TableMappings.Core/TableImport/IRowSaver.cs
+++ mrigrek74.TableMappings.Core/TableImport/IRowSaver.cs
@@ -4,7 +4,6 @@ namespace mrigrek74.TableMappings.Core.TableImport
 {
     public interface IRowSaver<in T> : IDisposable
     {
-        event EventHandler<DocumentImportEventArgs> Progress;
         void SaveRow(T row);
         void SaveRemainder();
     }
